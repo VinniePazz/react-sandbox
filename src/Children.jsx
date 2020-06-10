9539c1138cbb97057e0476fbb8392d3e869c0358
dimas <dimasmakouz@gmail.com>
2020-06-10 15:50:28 +0300

    test prettier

--- src/Children.jsx
+++ src/Children.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, memo } from "react"
 
 const Parent = memo((props) => {
-  const [state, setState] = useState({ text: "num: " });console.log("Parent");console.log(props.children)
+  const [state, setState] = useState({ text: "num: " })
+  console.log("Parent")
+  console.log(props.children)
   useEffect(() => {
     console.log("Parent useEffect")
   })
